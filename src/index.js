cd52de64693d262dc117654f204387c82cc08c05
patrickthesailorman <patrickthesaiorman@gmail.com>
2017-01-31 19:24:55 -0500

    response

--- src/index.js
+++ src/index.js
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './App.css';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+class Input extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+      valid: true
+    }
+  }
+
+  handleEmail (e) {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  handlePassword (e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  handlePasswordConfirm (e) {
+    this.setState({
+      passwordConfirm: e.target.value
+    })
+  }
+
+  handleSubmit (e) {
+    e.preventDefault()
+    this.checkValidity()
+  }
+
+  checkValidity () {
+    if (this.state.password === this.state.passwordConfirm) {
+        this.setState({ valid: true })
+    } else {
+      this.setState({ valid: false })
+    }
+  }
+
+  render () {
+    let validText = this.state.valid ? "Valid!" : "Passwords do not match"
+    return (
+        <div>
+          <input onChange={(e) => this.handleEmail(e)} type="text" placeholder="email" />
+          <input onChange={(e) => this.handlePassword(e)} type="password" placeholder="password" />
+          <input onChange={(e) => this.handlePasswordConfirm(e)} type="password" placeholder="confirm password" />
+          <input type="submit" onClick={(e) => this.handleSubmit(e)} value="submit" />
+          <p>{validText}</p>
+      </div>
+    );
+  }
+}
+
+export default Index
